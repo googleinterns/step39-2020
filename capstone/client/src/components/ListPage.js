c5b0b21769d2a3042bbddacc5149dc9ed9611d42
Anudeep Yakkala <ayakkala@purdue.edu>
2020-06-22 15:43:32 -0700

    Add comment explaing what the class does.

--- capstone/client/src/components/ListPage.js
+++ capstone/client/src/components/ListPage.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+
+/*
+ * This class displays a checkbox list containing the items returned from the Items API.
+ */
 class ListPage extends Component {
   render() {
     return (
