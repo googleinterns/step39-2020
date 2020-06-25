039a26ac5c555d792dcdf24fb3ac8c5d3a91b7b2
Anudeep <ayakkala@purdue.edu>
2020-06-25 23:54:38 +0000

    Add additional error handling.

--- capstone/backend/src/main/java/com/google/sps/servlets/CreateOrUpdateUserlistServlet.java
+++ capstone/backend/src/main/java/com/google/sps/servlets/CreateOrUpdateUserlistServlet.java
@@ -1,18 +1,18 @@
 package com.google.sps.servlets;
 
-import com.google.gson.Gson; 
+import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.List;
 
 /**
-* Servlet that creates or updates user lists. 
-*/
+ * Servlet that creates or updates user lists.
+ */
 @WebServlet("/api/v1/create-or-update-user-list-servlet")
 public class CreateOrUpdateUserlistServlet extends HttpServlet {
   private static final int OK = 200;
@@ -29,28 +29,33 @@ public class CreateOrUpdateUserlistServlet extends HttpServlet {
     private String displayName;
     private List<String> itemTypes;
   }
- 
- @Override
- public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-   Gson g = new Gson();
-   SpannerUtilFunctions suf = new SpannerUtilFunctions();
-   try {
-     RequestBody requestBody = getRequestBody(request);
-   } catch (IOException) {
-      response.setStatus(BAD_REQUEST)
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Gson g = new Gson();
+    SpannerUtilFunctions suf = new SpannerUtilFunctions();
+    try {
+      RequestBody requestBody = getRequestBody(request);
+      if (responseBody == null) {
+        response.setStatus(BAD_REQUEST);
+        response.getWriter.println("");
+      }
+    } catch (IOException e) {
+      response.setStatus(BAD_REQUEST);
       response.getWriter().println("");
       return;
-   }
-   try {
-     suf.writeUserLists(requestBody.userId. requestBody.UserList.listId, requestBody.UserList.itemTypes);
-   } catch (Exception e) {
-     response.setStatus(INTERNAL_SERVER_ERROR);
-     response.getWriter().println("");
-     return;
-   }
-   response.setStatus(OK);
-   response.getWriter().println("");
- }
+    }
+    try {
+      suf.writeUserLists(
+          requestBody.userId.requestBody.UserList.listId, requestBody.UserList.itemTypes);
+    } catch (Exception e) {
+      response.setStatus(INTERNAL_SERVER_ERROR);
+      response.getWriter().println("");
+      return;
+    }
+    response.setStatus(OK);
+    response.getWriter().println("");
+  }
 
   private RequestBody getRequestBody(HttpServletRequest request) throws IOException {
     Gson g = new Gson();
@@ -58,9 +63,15 @@ public class CreateOrUpdateUserlistServlet extends HttpServlet {
     String reqString = "";
     String line;
 
-    while((line = br.readLine()) != null) {
+    while ((line = br.readLine()) != null) {
       reqString += line;
     }
-    return g.fromJson(reqString, RequestBody.class);
+    ResponseBody responseBody = g.fromJson(reqString, RequestBody.class);
+    if (responseBody == null || requestBody.userId == 0 || requestBody.userList == null
+        || requestBody.userList.listId == 0 || requestBody.userList.displayName == null
+        || requestBody.userList.itemTypes == null) {
+      return null;
+    }
+    return responseBody;
   }
 }
