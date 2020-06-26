e990c1339060c62b5c5147d1f2ace812f2f50ae0
Anudeep <ayakkala@purdue.edu>
2020-06-26 00:49:36 +0000

    Add tests for CreateOrUpdateUserlist servlet.

---
+++ capstone/backend/src/test/java/com/google/sps/CreateOrUpdateUserlistServletTest.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.StringWriter;
+import java.io.StringReader;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import junit.framework.TestCase;
+import org.mockito.Mockito;
+
+public class CreateOrUpdateUserlistServletTest extends TestCase {
+  public void testDoPostSucceed() throws ServletException, IOException {
+    HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
+    HttpServletResponse res = Mockito.mock(HttpServletResponse.class);
+    StringWriter writer = new StringWriter();
+    StringReader reader = new StringReader(
+        "{\"userId\": 1,\"userList\":{\"listId\":2,\"displayName\":\"List Name\",\"itemTypes\":[\"butter\",\"Orange Juice\", \"Peanuts\"]}");
+    CreateOrUpdateUserlistServlet servlet = new CreateOrUpdateUserlistServlet();
+
+    Mockito.when(req.getMethod()).thenReturn("POST");
+    Mockito.when(req.getReader()).thenReturn(new BufferedReader(reader));
+    Mockito.when(res.getWriter()).thenReturn(new PrintWriter(writer));
+
+    servlet.doPost(req, res);
+    Mockito.verify(res).setStatus(HttpServletResponse.SC_OK);
+    String result = writer.toString();
+    assertTrue("Should be empty", result.equals(""));
+  }
+
+  public void testDoPostBadRequest() throws ServletException, IOException {
+    HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
+    HttpServletResponse res = Mockito.mock(HttpServletResponse.class);
+    StringWriter writer = new StringWriter();
+    StringReader reader = new StringReader("{\"bad response\" list:\"bread\"}");
+    CreateOrUpdateUserlistServlet servlet = new CreateOrUpdateUserlistServlet();
+
+    Mockito.when(req.getMethod()).thenReturn("POST");
+    Mockito.when(req.getReader()).thenReturn(new BufferedReader(reader));
+    Mockito.when(res.getWriter()).thenReturn(new PrintWriter(writer));
+
+    servlet.doPost(req, res);
+
+    Mockito.verify(res).setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    String result = writer.toString();
+    assertTrue("Should be empty", result.equals(""));
+  }
+}
