8d47afa9a3655972a5f73e3cab88b0bca64635ea
Anudeep Yakkala <ayakkala@purdue.edu>
2020-06-29 16:12:02 -0700

    Change visibility of UserList to public.

--- capstone/backend/src/main/java/com/google/servlets/UserList.java
+++ capstone/backend/src/main/java/com/google/servlets/UserList.java
@@ -2,7 +2,7 @@ package com.google.servlets;
 
 import java.util.List;
 
-class UserList {
+public class UserList {
   private long listId;
   private String displayName;
   private List<String> itemTypes;
