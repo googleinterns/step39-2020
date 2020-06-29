80be4e92fe3f913500cc3cda2845f9f2e2f7529f
Anudeep Yakkala <ayakkala@purdue.edu>
2020-06-29 16:08:25 -0700

    Add constructor.

--- capstone/backend/src/main/java/com/google/servlets/UserList.java
+++ capstone/backend/src/main/java/com/google/servlets/UserList.java
@@ -3,7 +3,13 @@ package com.google.servlets;
 import java.util.List;
 
 class UserList {
-    private long listId;
-    private String displayName;
-    private List<String> itemTypes;
+  private long listId;
+  private String displayName;
+  private List<String> itemTypes;
+
+  public UserList(long listId, String displayName, List<String> itemTypes) {
+    this.listId = listId;
+    this.displayName = displayName;
+    this.itemTypes = itemTypes;
+  }
 }
