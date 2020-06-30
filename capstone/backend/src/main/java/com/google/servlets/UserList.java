2046edbb9344a959197692c5a45b517a7f81d13c
Anudeep <ayakkala@purdue.edu>
2020-06-30 01:50:54 +0000

    Add a custom equals method to compare lists.

--- capstone/backend/src/main/java/com/google/servlets/UserList.java
+++ capstone/backend/src/main/java/com/google/servlets/UserList.java
@@ -12,4 +12,25 @@ public class UserList {
     this.displayName = displayName;
     this.itemTypes = itemTypes;
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+        return false;
+    }
+    if (!(obj instanceof UserList)) {
+        return false;
+    }
+    UserList userList = (UserList) obj;
+    if (userList.listId != this.listId) {
+        return false;
+    }
+    if (!userList.displayName.equals(this.displayName)) {
+        return false;
+    }
+    if (!userList.itemTypes.equals(this.itemTypes)) {
+        return false;
+    }
+    return true;
+  }
 }
