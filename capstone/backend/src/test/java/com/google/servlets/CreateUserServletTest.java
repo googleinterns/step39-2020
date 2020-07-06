3019d29981ffccd6a390fed106fa30b0c22d0315
Anudeep Yakkala <ayakkala@purdue.edu>
2020-07-06 12:34:25 -0700

    Modify to take an id token that is verified and used to obtain user information.

--- capstone/backend/src/test/java/com/google/servlets/CreateUserServletTest.java
+++ capstone/backend/src/test/java/com/google/servlets/CreateUserServletTest.java
@@ -1,20 +1,34 @@
 package com.google.servlets;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import junit.framework.TestCase;
 import org.mockito.Mockito;
 
 public class CreateUserServletTest extends TestCase {
-  private static final String CORRECT_REQUEST_STRING = 
-      "{\"email\": \"testing@gmail.com\",\"userName\":\"test user\"}";
+  private static final String ID_TOKEN_STRING = "testIdTokenString";
+  private static final String CORRECT_REQUEST_STRING = "{\"idTokenString\": \"" + ID_TOKEN_STRING + "\"}";
   private static final String INCORRECT_REQUEST_STRING = "{list:\"bread\"}";
 
-  public void testDoPostSucceed() throws ServletException, IOException {
+  public void testDoPostSucceed() throws ServletException, IOException, GeneralSecurityException {
     SetupObj setupObj = ServletTestUtil.setupMockDataPost(CORRECT_REQUEST_STRING);
+    Payload payload = Mockito.mock(Payload.class);
+    GoogleIdToken idToken = Mockito.mock(GoogleIdToken.class);
+    GoogleIdTokenVerifier verifier = Mockito.mock(GoogleIdTokenVerifier.class);
+    Mockito.when(payload.getSubject()).thenReturn("abcd1234");
+    Mockito.when(payload.getEmail()).thenReturn("test@gmail.com");
+    Mockito.when(payload.get("name")).thenReturn("Test User");
+    Mockito.when(idToken.getPayload()).thenReturn(payload);
+    Mockito.when(verifier.verify(ID_TOKEN_STRING)).thenReturn(idToken);
 
     CreateUserServlet servlet = new CreateUserServlet();
+    servlet.setVerifier(verifier);
     servlet.doPost(setupObj.request, setupObj.response);
 
     Mockito.verify(setupObj.response).setStatus(HttpServletResponse.SC_OK);
