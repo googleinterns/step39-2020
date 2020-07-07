4774ede30e02b530a6a8cfe3f9c45cd34c4cc463
Anudeep <ayakkala@purdue.edu>
2020-07-07 21:45:11 +0000

    Add Apache License.

--- capstone/backend/src/main/java/com/google/servlets/CreateUserServlet.java
+++ capstone/backend/src/main/java/com/google/servlets/CreateUserServlet.java
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.servlets;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.google.cloud.spanner.SpannerException;
 import com.google.gson.Gson;
 import com.google.spanner.LibraryFunctions;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,12 +36,10 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/api/v1/create-user")
 public class CreateUserServlet extends HttpServlet {
-  private GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-        .build();
+  private GoogleIdTokenVerifier verifier =
+      new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory()).build();
 
-  private class RequestBody {
-    String idTokenString;
-  }
+  private class RequestBody { String idTokenString; }
 
   private class ResponseBody {
     long userId;
@@ -95,7 +108,7 @@ public class CreateUserServlet extends HttpServlet {
     this.verifier = verifier;
   }
 
-  public void createUser(long userId, String userName, String email) throws SpannerException{
+  public void createUser(long userId, String userName, String email) throws SpannerException {
     LibraryFunctions.createUser(userId, userName, email);
   }
 }
--- capstone/backend/src/main/java/com/google/servlets/GetUserListsServlet.java
+++ capstone/backend/src/main/java/com/google/servlets/GetUserListsServlet.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.servlets;
 
 import com.google.cloud.spanner.SpannerException;
-import com.google.gson.Gson; 
+import com.google.gson.Gson;
 import com.google.spanner.LibraryFunctions;
 import java.io.IOException;
 import java.util.List;
@@ -21,14 +37,14 @@ public class GetUserListsServlet extends HttpServlet {
   }
 
   /*
-   * This handles a GET request to "/get-user-lists". The user's saved lists 
-   * are returned. 
+   * This handles a GET request to "/get-user-lists". The user's saved lists
+   * are returned.
    */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Gson g = new Gson();
     String userIdString = request.getParameter("userId");
-    
+
     if (userIdString == null || userIdString == "") {
       response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid request syntax.");
       return;
@@ -37,9 +53,9 @@ public class GetUserListsServlet extends HttpServlet {
     List<UserList> userLists;
     try {
       userLists = getUserLists(userId);
-    } catch (SpannerException se){
+    } catch (SpannerException se) {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
-        "An error occured while retriving data from the database.");
+          "An error occured while retriving data from the database.");
       return;
     }
 
