8f7b3986b7f897ad043b02fe6cf3463e55049e13
Anudeep Yakkala <ayakkala@purdue.edu>
2020-07-22 09:57:37 -0700

    Move items into expandable item type catogories and sort each catogory by price.

--- capstone/client/src/components/StoreDetailCards.js
+++ capstone/client/src/components/StoreDetailCards.js
@@ -16,7 +16,8 @@
 
 import React, { Component } from 'react';
 import { GoogleApiWrapper, Map } from 'google-maps-react';
-import { Card, Grid, List, ListItem, ListItemText, Typography } from '@material-ui/core';
+import { Accordion,  AccordionDetails, AccordionSummary, Card, Grid, List, ListItem, ListItemText, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import APIKey from './APIKey.js';
 import { StoresContext } from './StoresProvider.js';
@@ -38,19 +39,25 @@ class StoreDetailCards extends Component {
             </Map>
           </Grid>
           <Grid item component={Card} xs>
-            <Typography variant='subtitle1'>Has:</Typography>
-            <List>
+            <Typography variant='subtitle1'>This store has the following items:</Typography>
               {Object.keys(store.items).map((itemType, i) => (
-                <div>{Object.keys(store.items[itemType]).map((index, i) => (
-                  <ListItem key = {i}>
-                  <ListItemText>
-                    {store.items[itemType][index].itemName} (${(store.items[itemType][index].itemPrice-.005).toFixed(2)})
-                  </ListItemText>
-                </ListItem>
-                ))}
-                </div>
+                <Accordion>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography variant='subtitle1'>{itemType}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <List>
+                    {Object.keys(store.items[itemType].sort((a, b) => a.itemPrice - b.itemPrice)).map((index, i) => (
+                      <ListItem key = {i}>
+                        <ListItemText>
+                          {store.items[itemType][index].itemName} (${(store.items[itemType][index].itemPrice-.005).toFixed(2)})
+                        </ListItemText>
+                      </ListItem>
+                    ))}
+                    </List>
+                  </AccordionDetails>
+                </Accordion>
               ))}
-            </List>
           </Grid>
         </Grid>
       </div>
