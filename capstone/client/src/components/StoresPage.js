fa29f06ba0047293b8bdfca203b9441d71804342
brettallenyo <bzallen@google.com>
2020-07-21 20:48:17 -0700

    Back button

--- capstone/client/src/components/StoresPage.js
+++ capstone/client/src/components/StoresPage.js
@@ -15,7 +15,8 @@
  */
 
 import axios from 'axios';
-import { Card, Grid } from '@material-ui/core';
+import {Redirect} from 'react-router-dom';
+import { Button, Card, Grid } from '@material-ui/core';
 import React, { Component } from 'react';
 
 import { Store } from './Store';
@@ -37,6 +38,7 @@ class StorePage extends Component {
           distanceValue : params.get('distanceValue'),
           latitude : params.get('latitude'),
           longitude : params.get('longitude'),
+          redirect : null,
       }
       this.getStores = this.getStores.bind(this);
     }
@@ -61,6 +63,12 @@ class StorePage extends Component {
         });
     }
 
+    goBack = () => {
+      this.setState({
+        redirect : "/",
+      });
+    }
+
   handleFilterchange = (stores) => {
     this.setState({
       stores,
@@ -68,10 +76,17 @@ class StorePage extends Component {
   }
 
   render() {
+    if(this.state.redirect) {
+      return <Redirect to={{
+        pathname : this.state.redirect,
+      }}/>
+    }
+
     return(
-      <div>
+      <div id="stores-page-container">
         <h1>Store Recommendations</h1>
         <StoresProvider>
+        <Button id="back-button" onClick={this.goBack} color="primary" variant="contained">Back To List</Button>
         <Grid container alignItems="stretch">
           <Grid item component={Card} xs>
             <FilterStores originalStores={this.state.originalStores} items={this.state.items} onFilterChange={this.handleFilterchange}/>
