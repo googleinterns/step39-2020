7c41b18072830eec0d6c67353f5819ded48ef7fe
officialcwang <carolynw@gmail.com>
2020-07-28 12:02:13 -0700

    Add a footer to WelcomePage.

---
+++ capstone/client/src/Footer.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2020 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { Component } from 'react';
+//import Footer from 'rc-footer';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { withRouter } from "react-router-dom";
+
+function Footer() {
+    return(
+      <div>
+        <h1>Footer!</h1>
+      </div>
+    );
+}
+
+export default Footer;
