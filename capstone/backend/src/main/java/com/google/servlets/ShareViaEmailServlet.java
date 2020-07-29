c376e20b63d756bb6d0f7a789df03f015890d0af
Brett Allen <bzallen@google.com>
2020-07-29 18:12:47 +0000

    functioning email

--- capstone/backend/src/main/java/com/google/servlets/ShareViaEmailServlet.java
+++ capstone/backend/src/main/java/com/google/servlets/ShareViaEmailServlet.java
@@ -2,10 +2,10 @@ package com.google.servlets;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Properties;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -41,47 +41,27 @@ public class ShareViaEmailServlet extends HttpServlet {
     }
     try {
       sendEmail(requestBody.email, requestBody.html);
-    } catch (MessagingException e) {
-      response.sendError(
-          HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occured while sending email.");
+    } catch (Exception e) {
+      StringWriter sw = new StringWriter();
+      PrintWriter pw = new PrintWriter(sw);
+      e.printStackTrace(pw);
+      String sStackTrace = sw.toString();
+      response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, sStackTrace);
       return;
     }
     response.setStatus(HttpServletResponse.SC_OK);
     response.setContentType("application/json;");
   }
 
-  public void sendEmail(String email, String html) throws MessagingException {
-    // Recipient's email ID needs to be mentioned.
-    String to = email;
-    // Sender's email ID needs to be mentioned
-    String from = "shopsmart.step@gmail.com";
-    // Assuming you are sending email from through gmails smtp
-    String host = "smtp.gmail.com";
-    // Get system properties
-    Properties properties = System.getProperties();
-    // Setup mail server
-    properties.put("mail.smtp.host", host);
-    properties.put("mail.smtp.port", "465");
-    properties.put("mail.smtp.ssl.enable", "true");
-    properties.put("mail.smtp.auth", "true");
-    // Get the Session object.// and pass username and password
-    Session session =
-        Session.getInstance(
-            properties,
-            new javax.mail.Authenticator() {
-              protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(
-                    GmailCredentials.getUsername(), GmailCredentials.getPassword());
-              }
-            });
-    // Used to debug SMTP issues
-    session.setDebug(true);
+  public void sendEmail(String email, String html) throws Exception {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
     // Create a default MimeMessage object.
-    MimeMessage message = new MimeMessage(session);
+    Message message = new MimeMessage(session);
     // Set From: header field of the header.
-    message.setFrom(new InternetAddress(from));
+    message.setFrom(new InternetAddress("shopsmart@step39-2020.appspotmail.com", "Shopsmart"));
     // Set To: header field of the header.
-    message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+    message.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
     // Set Subject: header field
     message.setSubject("Message From ShopSmart");
     // Now set the actual message
